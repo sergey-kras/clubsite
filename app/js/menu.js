30a955f1fcf24fdbee1d27d2dd974b84ecf83735
SergeyKR <vip.41243@gmail.com>
2019-01-15 14:06:03 +0700

    Before fix bags

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     var $menuToggle = $("#menuToggle");
     var $menuBox = $("#menuBox");
+    var $menuHeader = $("#header");
 
     $menuToggle.click(function(event){
         $menuToggle.toggleClass("_menu_open");
+        $menuHeader.toggleClass("_menu_open");
+        $menuBox.toggleClass("_menu_open");
+        $(".iconText._menuLink").toggleClass("_menu_open");
     });
 });
